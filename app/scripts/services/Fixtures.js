b74fe854e58e481c43e5cd432894c2a3f8488ccb
Rob Palmer <robpalmer@Robs-MacBook-Pro-2.local>
2017-02-14 16:43:35 -0500

    assignment6 complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -34,7 +34,15 @@
         
          Fixtures.getAlbum = function() {
         return albumPicasso;
-        };  
+        };
+         
+         Fixtures.getCollection = function(numberOfAlbums) {
+             this.albums = []
+             for (var i=0 ; i < numberOfAlbums ; i++){
+                 this.albums.push(angular.copy(albumPicasso));
+             }
+             return this.albums
+         };
          
         return Fixtures;
 }
